01088931d527113f8a3d596ec01d46d1b7cc41d1
analunasnowflake5 <105214278+analunasnowflake5@users.noreply.github.com>
2022-05-09 15:48:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach ¬ Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
-streamlit.header ('Build Your Own Fruit Smothie')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
